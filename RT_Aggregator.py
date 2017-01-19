547e61c896ce0a28dc6171e0598ce9d7a350fba7
Jonathan Bard <jbard@localhost>
2017-01-19 12:41:10 -0500

    Added things

--- RT_Aggregator.py
+++ RT_Aggregator.py
@@ -3,6 +3,9 @@
 # This script loads metrics into RequestTracker tickets semi-automatically.
 # JB and JK 01-18-17
 
+import sys
+from xml.dom import minidom
+
 #Class that will implement parsing of Rapid Runs
 class RapidRunParser():
     def __init__(self):
@@ -23,9 +26,9 @@ class MiSeqRunParser():
         print 'Parsing as MiSeq Run'
     def collectCV(self):
 	#TODO : Return CV metrics for a miseq-run here
-        print "Needs implementation"
+        print "MiSeq -- Needs implementation"
     def collectAlign(self):
-        print "Needs implementation"
+        print "MiSeq -- Needs implementation"
 
 # Class that will eventually implement parsing of high-output runs
 class HighOutputRunParser():
@@ -55,9 +58,33 @@ class DataParser:
 
 # Main Method
 if __name__ == '__main__':
+
+    #Set up our abstract parsing object which will later be assigned to a flowcell parsing strategy
     parser = DataParser()
-    flowcell_type = RapidRunParser()
+
+    # Capture the directory from the command line
+
+    file = sys.argv[1]
+    found = file.rstrip() + '/Files/RunInfo.xml'#Parameters.xml'
+    xmldoc = minidom.parse(found)
+    element = xmldoc.getElementsByTagName('FlowcellLayout')
+
+    LC = (element[0].attributes['LaneCount'].value)
+    SC = (element[0].attributes['SurfaceCount'].value)
+    flowcell_type = 0
+    if( LC=="8"):
+        flowcell_type = HighOutputRunParser()
+    elif(LC=="2"):
+        flowcell_type = RapidRunParser()    
+    elif(LC=="1" and SC =="2"):
+        flowcell_type = MiSeqRunParser()
+    elif(LC =="1" and SC =="1"):
+        flowcell_type = MiSeqRunParser()
+
     parser.setFlowcellType(flowcell_type)
     parser.parser_test()
+    parser.collectCV()
+    parser.collectAlign()
+
 
 
