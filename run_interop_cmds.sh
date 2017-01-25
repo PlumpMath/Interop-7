95f89a78e3afcd766d2d2912c25608d1f4519e4f
Jonathan Bard <jbard@localhost>
2017-01-25 14:32:11 -0500

    All

---
+++ run_interop_cmds.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Run the summary and index-summary scripts for a flowcell
+
+fname=`basename $1`
+
+/data/apps/interop-buld/src/apps/summary /data/BaseSpace/Runs/$fname/Files | sed 's/ \+//g'  | grep -A 100 "Total" > /data/basemount-processing/$fname.summary.csv
+/data/apps/interop-buld/src/apps/index-summary /data/BaseSpace/Runs/$fname/Files | sed 's/ \+/\t/g' |  grep -A 2 "Lane"  > /data/basemount-processing/$fname.index-summary.csv
+python RT_Aggregator.py $fname
+
+
